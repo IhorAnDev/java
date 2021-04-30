82624da6ad2adaae1f4f4460d518750827d3f0d4
Antonov Igor <Gosha.8686@gmail.com>
2021-04-30 18:21:59 +0300

    ternar

--- src/Lesson12/Car.java
+++ src/Lesson12/Car.java
@@ -4,24 +4,24 @@ public class Car {
     int engine;
     int doorCount;
 
-    Car(int engine,int doorCount){
+    Car(int engine, int doorCount) {
         this.engine = engine;
         this.doorCount = doorCount;
     }
 }
 
-class CarTest{
+class CarTest {
     public static void main(String[] args) {
-        Car c1 = new Car(3,4);
-        Car c2 = new Car(2,5);
+        Car c1 = new Car(3, 4);
+        Car c2 = new Car(2, 5);
 
-        if(c1.engine>c2.engine){
-            if (c1.doorCount>c2.doorCount){
+        if (c1.engine > c2.engine) {
+            if (c1.doorCount > c2.doorCount) {
                 System.out.println("Moshost i dverey u pervoibolshe ");
-            }else {
+            } else {
                 System.out.println("Moshnost u pervoy bolshe a dverey menshe ");
             }
-        }else {
+        } else {
             System.out.println("Moshnost motora u pervoy mashini menshe ");
         }
     }
--- src/Lesson12/Test10.java
+++ src/Lesson12/Test10.java
@@ -2,17 +2,23 @@ package Lesson12;
 
 public class Test10 {
 
-    public static void main(String[] args) {
-        int salary = 600;
-
-
-        if (salary >= 500) {
-            System.out.println(":-)");
-        } else {
-            System.out.println(":-(");
+   static void abc() {
+        String str;
+        int a = 5;
+        if (a >= 10) {
+            str = "Privet";
         }
+        if (a < 1) {
+            str = "Poka";
+        }else {
+            str="tut str";
+        }
+        System.out.println(str);
+    }
 
+    public static void main(String[] args) {
 
+       abc();
 
     }
 }
