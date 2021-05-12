6ae9ad2370a07b67dc6cf1ae1bc71c316777d91c
Antonov Igor <Gosha.8686@gmail.com>
2021-05-12 18:27:22 +0300

    less 24

--- src/Lesson24/Test2.java
+++ src/Lesson24/Test2.java
@@ -1,10 +1,14 @@
 package Lesson24;
 
 public class Test2 {
+    public static void main(String[] args) {
+        Figura f1 = new Kvadrat();
+        System.out.println(f1.kolichestvostoroni);
+    }
 }
 
 abstract class Figura {
-    int storoni = 0;
+    int kolichestvostoroni = 0;
 
     abstract void perimetr();
 
@@ -16,6 +20,8 @@ abstract class Figura {
 }
 
 class Kvadrat extends Figura {
+
+    int kolichestvostoroni = 4;
     int storona1 = 10;
 
     @Override
@@ -31,26 +37,38 @@ class Kvadrat extends Figura {
 
 class Pramougolnik extends Figura {
 
+    int storoni = 4;
+    int storona1 = 8;
+    int storona2 = 5;
+
     @Override
     void perimetr() {
-
+        System.out.println("Perimetr pramougolnika = " + 2 * (storona1 + storona2));
     }
 
     @Override
     void ploshad() {
-
+        System.out.println("Ploshad pramougolnika = " + storona1 * storona2);
     }
 }
 
 class Okrujnost extends Figura {
+    int storoni = 0;
+    int radius = 3;
 
     @Override
     void perimetr() {
-
+        System.out.println("Perimetr okruzhosti = " + 2 * 3.14 * radius);
     }
 
     @Override
     void ploshad() {
+        System.out.println("Ploshad okruzhosti = " + 3.14 * radius * radius);
+    }
+}
 
+abstract class Chetirexuogolnik extends Figura {
+    void def() {
+        System.out.println("Eto ceturexugolnik");
     }
 }
