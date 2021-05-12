cf0a640777e9fa200714f2c23d04cc1e63bcc497
Antonov Igor <Gosha.8686@gmail.com>
2021-05-12 18:04:43 +0300

    conflict

--- src/Lesson23/Test3.java
+++ src/Lesson23/Test3.java
@@ -1,92 +1,52 @@
-package Lesson23;
-
-<<<<<<< HEAD
-public class Test3 {
-    public static void main(String[] args) {
-        Teacher teacher = new Teacher();
-        teacher.eat();
-    }
-}
-
-class Eda {
-}
-
-class Frukt extends Eda {
-
-}
-
-class Employee {
-    double salary = 100;
-=======
-
-public class Test3 {
-    public static void main(String[] args) {
-        Employee e = new Teacher();
-        e.eat();
-    }
-}
-
+//package Lesson23;
+//
+//
+//public class Test3 {
+//    public static void main(String[] args) {
+//        Employee e = new Teacher();
+//        e.eat();
+//    }
+//}
+//
+//class Eda {
+//}
+//
+//class Fruit extends Eda {
+//}
+//
+//class Employee {
+//    double salary;
+//    String name = "Kolya";
+//    int age;
+//    int experience;
+//
+//     public Eda eat() {
+//        System.out.println("kushaet rabotnik");
+//        Eda e = new Eda();
+//        return e;
+//    }
+//
+//    void sleep() {
+//        System.out.println("Spat");
+//
+//    }
+//}
+//
+//class Teacher extends Employee {
+//
+//    int kolvouchenikov;
+//
+//   public Eda eat() {
+//        System.out.println("kushaet uchitel");
+//        Fruit f = new Fruit();
+//        return f;
+//    }
+//
+//    void uchit() {
+//        System.out.println("Uchit");
+//    }
+//}
 /*
-class Eda {
-}
-
-class Fruit extends Eda {
-}
-
-class Employee {
-    double salary;
->>>>>>> refs/remotes/origin/main
-    String name = "Kolya";
-    int age;
-    int experience;
-
-<<<<<<< HEAD
-    Eda eat() {
-        System.out.println("Eating employee");
-=======
-     public Eda eat() {
-        System.out.println("kushaet rabotnik");
->>>>>>> refs/remotes/origin/main
-        Eda e = new Eda();
-        return e;
-    }
-
-    void sleep() {
-<<<<<<< HEAD
-        System.out.println("SLEEPING");
-=======
-        System.out.println("Spat");
-
->>>>>>> refs/remotes/origin/main
-    }
-}
-
-class Teacher extends Employee {
-<<<<<<< HEAD
-    int ucheniki;
-
-    Frukt eat() {
-        System.out.println("Eating Uchitel");
-        Frukt frukt = new Frukt();
-        return frukt;
-=======
-    int kolvouchenikov;
-
-   public Eda eat() {
-        System.out.println("kushaet uchitel");
-        Fruit f = new Fruit();
-        return f;
->>>>>>> refs/remotes/origin/main
-    }
-
-    void uchit() {
-        System.out.println("Uchit");
-    }
-<<<<<<< HEAD
-}
-=======
-}
-
 class A {
     Employee objCreat() {
         return new Employee();
@@ -100,4 +60,3 @@ class B extends A {
     }
 }
 */
->>>>>>> refs/remotes/origin/main
