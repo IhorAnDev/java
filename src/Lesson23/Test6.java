628d5bf5c0b8b1ce96b5ba37f2856ebedc1276ac
Antonov Igor <Gosha.8686@gmail.com>
2021-05-12 19:53:44 +0300

    compile bind

--- src/Lesson23/Test6.java
+++ src/Lesson23/Test6.java
@@ -1,30 +1,30 @@
-package Lesson23;
-
-public class Test6 {
-
-}
-
-class Animal {
-     String showName() {
-        return "Some animal";
-    }
-
-     void showIfoAboutAnimal() {
-        System.out.println("Name of animal: " + showName());
-    }
-}
-
-class Mouse extends Animal {
-     String showName() {
-        return "Jerry";
-    }
-    void showIfoAboutMouse() {
-        System.out.println("Name of mouse: " + showName());
-    }
-
-    public static void main(String[] args) {
-        Mouse a = new Mouse();
-        a.showIfoAboutAnimal();
-        a.showIfoAboutMouse();
-    }
-}
+//package Lesson23;
+//
+//public class Test6 {
+//
+//}
+//
+//class Animal {
+//     String showName() {
+//        return "Some animal";
+//    }
+//
+//     void showIfoAboutAnimal() {
+//        System.out.println("Name of animal: " + showName());
+//    }
+//}
+//
+//class Mouse extends Animal {
+//     String showName() {
+//        return "Jerry";
+//    }
+//    void showIfoAboutMouse() {
+//        System.out.println("Name of mouse: " + showName());
+//    }
+//
+//    public static void main(String[] args) {
+//        Mouse a = new Mouse();
+//        a.showIfoAboutAnimal();
+//        a.showIfoAboutMouse();
+//    }
+//}
