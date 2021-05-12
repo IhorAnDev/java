31b60b2949faee2ce5865c8e0aa42a061504c52a
Antonov Igor <Gosha.8686@gmail.com>
2021-05-12 18:08:13 +0300

    work space

--- src/Lesson23/Test2.java
+++ src/Lesson23/Test2.java
@@ -1,75 +1,72 @@
-package Lesson23;
-
-
-public class Test2 {
-    public Object abc() {
-        return new String("String");
-    }
-
-    public static void main(String[] args) {
-//        Doctor doctor = new Doctor();
-//        Teacher teacher = new Teacher();
-//        Employee employee = new Employee();
-//        Xirurg xirurg = new Xirurg();
-
-        Employee employee1 = new Doctor();
-        System.out.println(employee1.salary);
-        System.out.println(employee1.age);
-        System.out.println(employee1.experience);
-        employee1.eat();
-        employee1.sleep();
-
-        Employee employee2 = new Teacher();
-        Doctor doctor = new Xirurg();
-        System.out.println(doctor.age);
-        doctor.lechit();
-
-        Employee employee3 = new Xirurg();
-
-    }
-}
-
-<<<<<<< HEAD
- class Employe {
-=======
-class Employe {
->>>>>>> refs/remotes/origin/main
-    double salary = 100;
-    String name = "Kolya";
-    int age;
-    int experience;
-
-    void eat() {
-        System.out.println("Kushat");
-    }
-
-    void sleep() {
-        System.out.println("Spat");
-    }
-}
-
-class Doctor extends Employee {
-    void lechit() {
-        System.out.println("lechit");
-    }
-}
-
-class Xirurg extends Doctor {
-    String skalpel;
-
-    void operacia() {
-        System.out.println("Operiruet");
-    }
-}
-
-class Teache extends Employee {
-    int kolvouchenikov;
-
-    void uchit() {
-        System.out.println("Uchit");
-    }
-
-    void vodit() {
-        System.out.println("Vodit");
-    }
-}
+//package Lesson23;
+//
+//
+//public class Test2 {
+//    public Object abc() {
+//        return new String("String");
+//    }
+//
+//    public static void main(String[] args) {
+////        Doctor doctor = new Doctor();
+////        Teacher teacher = new Teacher();
+////        Employee employee = new Employee();
+////        Xirurg xirurg = new Xirurg();
+//
+//        Employee employee1 = new Doctor();
+//        System.out.println(employee1.salary);
+//        System.out.println(employee1.age);
+//        System.out.println(employee1.experience);
+//        employee1.eat();
+//        employee1.sleep();
+//
+//        Employee employee2 = new Teacher();
+//        Doctor doctor = new Xirurg();
+//        System.out.println(doctor.age);
+//        doctor.lechit();
+//
+//        Employee employee3 = new Xirurg();
+//
+//    }
+//}
+//
+//
+//class Employe {
+//    double salary = 100;
+//    String name = "Kolya";
+//    int age;
+//    int experience;
+//
+//    void eat() {
+//        System.out.println("Kushat");
+//    }
+//
+//    void sleep() {
+//        System.out.println("Spat");
+//    }
+//}
+//
+//class Doctor extends Employee {
+//    void lechit() {
+//        System.out.println("lechit");
+//    }
+//}
+//
+//class Xirurg extends Doctor {
+//    String skalpel;
+//
+//    void operacia() {
+//        System.out.println("Operiruet");
+//    }
+//}
+//
+//class Teache extends Employee {
+//    int kolvouchenikov;
+//
+//    void uchit() {
+//        System.out.println("Uchit");
+//    }
+//
+//    void vodit() {
+//        System.out.println("Vodit");
+//    }
+//}
